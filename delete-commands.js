637595f3d4ca894077f74df2fc58b3b251858a8b
EAnathos <eanathos@gmail.com>
2022-10-15 00:46:06 +0200

    Create delete-commands.js
    Add possibility to delete slash commands

---
+++ delete-commands.js
@@ -0,0 +1,14 @@
+const { REST, Routes } = require('discord.js');
+const { CLIENT_ID, GUILD_ID, TOKEN } = require('./config.json');
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+// for guild-based commands
+rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: [] })
+	.then(() => console.log('Successfully deleted all guild commands.'))
+	.catch(console.error);
+
+// for global commands
+rest.put(Routes.applicationCommands(CLIENT_ID), { body: [] })
+	.then(() => console.log('Successfully deleted all application commands.'))
+	.catch(console.error);
