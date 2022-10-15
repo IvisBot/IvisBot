7d12d11a2b1d44ed3f19b4043f818f0ec93f2467
EAnathos <eanathos@gmail.com>
2022-10-15 13:43:02 +0200

    Update guildMemberAdd
    This event is not working at the moment

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,6 +1,38 @@
+const { WELCOME_CHANNEL_ID } = require('../config.json');
+const Canvas = require("canvas");
+
 module.exports = {
     name : 'guildMemberAdd',
-    execute(interaction) {
-		console.log(`${interaction.user.tag} join the server.`);
-	},
+    async execute(member) {
+		  console.log(`${member.user.tag} join the server.`);
+
+      var canvas = Canvas.createCanvas(1024, 500);
+
+      ctx = canvas.getContext("2d");
+
+      var background = await Canvas.loadImage("./images/welcomeBanner.jpg");
+      ctx.drawImage(background, 0, 0, 1024, 500);
+
+      ctx.font = "42px Impact";
+      ctx.fillStyle = "#ED4E2C";
+      ctx.textAlign = "center";
+      ctx.textBaseLine = "bottom";
+      ctx.fillText("Welcome " + member.user.tag, 512, 410);
+
+      ctx.beginPath();
+      ctx.arc(512, 166, 110, 0, Math.PI * 2);
+      ctx.closePath();
+      ctx.clip();
+
+      var avatar = await Canvas.loadImage(member.user.displayAvatarURL({
+          format: "jpg",
+          size: 1024
+      }));
+
+      ctx.drawImage(avatar, 393, 47, 238, 238);
+
+      var attachment = new Discord.MessageAttachment(canvas.toBuffer(), "welcome.png");
+
+      Client.channels.cache.get(WELCOME_CHANNEL_ID).send({files: [attachment]});
+    },
 };
