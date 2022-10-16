104e05eb9c239550a47cadd86141ae4952e05bbe
EAnathos <eanathos@gmail.com>
2022-10-16 22:52:02 +0200

    Update server-info.js

--- commands/server-info.js
+++ commands/server-info.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { EmbedBuilder } = require('discord.js');
+const { SERVER_DESCRIPTION } = require('../config.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -12,7 +13,43 @@ module.exports = {
 		const serverInfoEmbed = new EmbedBuilder()
 			.setColor('Purple')
 			.setTitle(interaction.guild.name)
-			.setDescription(`Total members: ${interaction.guild.memberCount}`)
+			.setDescription(`${SERVER_DESCRIPTION}`)
+			.addFields(
+				{
+					name : "Creation date :",
+					value : `${`\`${interaction.guild.createdAt}`.split(' ').slice(0, 4).join(" ")}\``,
+					inline : true
+				},
+				{
+					name : "Server owner :",
+					value : `<@${interaction.guild.ownerId}>`,
+					inline : true
+				},
+				{
+					name : '\u200b',
+					value : '\u200b',
+					inline : true
+				},
+				{
+					name : "Total members :",
+					value : `\`${interaction.guild.memberCount}\``,
+					inline : true
+				},
+				{
+					name : "Amount of nitro boost :",
+					value : `\`${interaction.guild.premiumSubscriptionCount}\``,
+					inline : true
+				},
+				{
+					name : '\u200b',
+					value : '\u200b',
+					inline : true
+				},
+				{
+					name : "Rule channel :",
+					value : `<#${interaction.guild.rulesChannelId}>`
+				}
+			)
 			.setAuthor({ name: 'Ivis', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
 			.setTimestamp()
 			.setFooter({ text: 'IvisBot, A fully modular Discord Bot', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
@@ -21,10 +58,7 @@ module.exports = {
 			serverInfoEmbed.setImage(icon)
 			serverInfoEmbed.setURL(icon)
 		} else {
-			serverInfoEmbed.addFields( 
-				{ name : "An error occured !",
-				value : "You server has no icon."}
-			)
+			serverInfoEmbed.addFields({ name : "An error occured !", value : "You server has no icon." })
 		};
 		
 		interaction.reply({ embeds: [serverInfoEmbed] });
