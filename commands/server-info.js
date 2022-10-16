3f8390688feac54355fe7484c531f805e42c684c
EAnathos <eanathos@gmail.com>
2022-10-16 17:32:46 +0200

    Update server-info.js

--- commands/server-info.js
+++ commands/server-info.js
@@ -1,10 +1,24 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('server-info')
 		.setDescription('Display info about this server.'),
 	async execute(interaction) {
-		return interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+
+		console.log(interaction.guild.iconURL())
+		
+		const serverInfoEmbed = new EmbedBuilder()
+			.setColor('Purple')
+			.setTitle(interaction.guild.name)
+			.setDescription(`Total members: ${interaction.guild.memberCount}`)
+			.setURL(interaction.guild.iconURL())
+			.setAuthor({ name: 'Ivis', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+			.setImage(interaction.guild.iconURL())
+			.setTimestamp()
+			.setFooter({ text: 'IvisBot, A fully modular Discord Bot', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+		
+		interaction.reply({ embeds: [serverInfoEmbed] });
 	},
 };
