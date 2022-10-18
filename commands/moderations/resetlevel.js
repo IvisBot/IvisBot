87488ecdadafcbe3734287f162edc5a262dbd334
EAnathos <eanathos@gmail.com>
2022-10-18 13:39:02 +0200

    add resetlevel.js

---
+++ commands/moderations/resetlevel.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+const Level = require('../../models/level.model');
+const levelModel = require('../../models/level.model');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('resetlevel')
+		.setDescription('Reset level of someone.')
+		.addUserOption((option) => option
+            .setName('pseudo')
+            .setDescription('The member nickname')
+            .setRequired(true)),
+	async execute(interaction) {
+		const pseudo = interaction.options.getUser('pseudo');
+
+        if (levelModel) {
+            const levelModel = await Level.findOne({ memberId: pseudo.id });
+            
+            if(levelModel) {
+                levelModel.level = 0;
+                levelModel.xp = 0;
+                levelModel.save();
+                interaction.reply({ content: `Level of ${pseudo.username} has been reset to 0`, ephemeral: true });
+            }
+        }
+        else {
+            interaction.reply("Error");
+        }
+	},
+};
