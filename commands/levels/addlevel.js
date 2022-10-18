39c4280b125b657767e292ba7ecaf26bbc1ceb3e
EAnathos <eanathos@gmail.com>
2022-10-18 17:43:50 +0200

    Create addlevel.js

---
+++ commands/levels/addlevel.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+const Level = require('../../models/level.model');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('addlevel')
+        .setDescription('Add level to someone.')
+        .addUserOption((option) => option
+            .setName('pseudo')
+            .setDescription('The member nickname')
+            .setRequired(true))
+        .addIntegerOption((option) => option
+            .setName('amount')
+            .setDescription('The amount of level to add')
+            .setRequired(true)),
+    async execute(interaction) {
+        const pseudo = interaction.options.getUser('pseudo');
+        const amount = interaction.options.getInteger('amount');
+
+        const levelModel = await Level.findOne({ memberId: pseudo.id });
+
+        try { if (levelModel) {
+            levelModel.level += amount;
+            levelModel.save();
+            interaction.reply({ content: `Level of ${pseudo.username} has been added by ${amount}`, ephemeral: true });
+        }} catch (error) {
+            console.log(error);
+            interaction.reply({ content: "Player not found", ephemeral: true });
+        }
+    }
+}
