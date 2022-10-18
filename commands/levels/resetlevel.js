2803de08f6db2e781a1898fa4dbd0f451b91124b
EAnathos <eanathos@gmail.com>
2022-10-18 17:17:28 +0200

    Update resetlevel.js

--- commands/levels/resetlevel.js
+++ commands/levels/resetlevel.js
@@ -1,7 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 const Level = require('../../models/level.model');
-const levelModel = require('../../models/level.model');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -13,19 +12,16 @@ module.exports = {
             .setRequired(true)),
 	async execute(interaction) {
 		const pseudo = interaction.options.getUser('pseudo');
-
-        if (levelModel) {
-            const levelModel = await Level.findOne({ memberId: pseudo.id });
-            
-            try { if(levelModel) {
-                levelModel.level = 0;
-                levelModel.xp = 0;
-                levelModel.save();
-                interaction.reply({ content: `Level of ${pseudo.username} has been reset to 0`, ephemeral: true });
-            }} catch (error) {
-                console.log(error);
-                interaction.reply({ content: "Player not found", ephemeral: true });
-            }
+        const levelModel = await Level.findOne({ memberId: pseudo.id });
+        
+        try { if(levelModel) {
+            levelModel.level = 0;
+            levelModel.xp = 0;
+            levelModel.save();
+            interaction.reply({ content: `Level of ${pseudo.username} has been reset to 0`, ephemeral: true });
+        }} catch (error) {
+            console.log(error);
+            interaction.reply({ content: "Player not found", ephemeral: true });
         }
 	},
 };
