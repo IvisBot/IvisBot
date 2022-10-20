63a891cce59db3bf185f3ffae75660ffb1d23729
Damien Cren <68796664+damiencren@users.noreply.github.com>
2022-10-21 01:02:23 +0200

    Update lol.js

--- commands/infos/lol.js
+++ commands/infos/lol.js
@@ -1,6 +1,7 @@
-const { RIOT_API, BOT_LOGO, BOT_TEXTFOOTER, BOT_LINKFOOTER } = require('../../config.json');
+const { RIOT_API, BOT_LOGO, BOT_TEXTFOOTER} = require('../../config.json');
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const axios = require('axios');
+var request = require('request');
 
 
 module.exports = {
@@ -12,31 +13,51 @@ module.exports = {
 
 		const profile = await axios.get(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${interaction.options.getString('player')}?api_key=${RIOT_API}`);
 		const masteries = await axios.get(`https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${profile.data.id}?api_key=${RIOT_API}`);
-		
 		const rank = await axios.get(`https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/${profile.data.id}?api_key=${RIOT_API}`);
-
 		console.log(profile.data);
 		console.log(rank.data)
-		console.log(masteries.data.slice(0,3));
-		console.log(profile.data.summonerLevel.toString());
+		const bestMasteries = masteries.data.slice(0,3);
+		console.log(bestMasteries);
 
 		const lolAmbed = new EmbedBuilder()
 			.setTitle(profile.data.name+ "'s stats")
+			.setAuthor({ name: 'Requested by '+interaction.user.tag, iconURL: 'https://developer.riotgames.com/static/img/katarina.55a01cf0560a.gif' })
 			.setThumbnail(`https://ddragon.leagueoflegends.com/cdn/11.16.1/img/profileicon/${profile.data.profileIconId}.png`)
-			.setColor('Blue')
+			.setColor('#a80016')
 			.addFields({name: 'Level', value: profile.data.summonerLevel.toString()},)
 			for (var i=0; i<rank.data.length; i++){
 				lolAmbed.addFields(
 					{name: rank.data[i].queueType, value: rank.data[i].tier+" : "+rank.data[i].rank, inline: true},)}
 
-			lolAmbed.setAuthor({ name: 'Requested by '+interaction.user.id, iconURL: 'https://i.imgur.com/AfFp7pu.png' })
-			.setTimestamp()
+			lolAmbed.addFields({name: 'Most Played Champs', value: profile.data.summonerLevel.toString(), inline: false})
+			
+			for (var i=0; i<bestMasteries.length; i++){
+				var champName = await getChampName(bestMasteries[i].championId);
+				lolAmbed.addFields({name: champName, value: profile.data.summonerLevel.toString(), inline: false})}
+
+			lolAmbed.setTimestamp()
 			.setFooter({ text: BOT_TEXTFOOTER, iconURL: BOT_LOGO});
 
 
 		interaction.reply({ embeds: [lolAmbed] });
 		
-    
+		async function getChampName(id) {
+			request('http://ddragon.leagueoflegends.com/cdn/11.16.1/data/de_DE/champion.json', function (error, response, body) {
+		  
+			  let list = JSON.parse(body);
+			  let championList = list.data;
+		  
+			  for (var i in championList) {
+		  
+				if (championList[i].key == id) {
+					console.log(championList[i].id);
+				  	return championList[i].id
+				}
+		  
+			  }
+		  
+			});
+		}
 	},
 
 };
