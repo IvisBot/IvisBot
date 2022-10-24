ddb96517054b3a0b6245e82fdb94c6b753063ba4
EAnathos <eanathos@gmail.com>
2022-10-24 11:14:37 +0200

    Create guildEmojiUpdate.js

---
+++ events/guild/emoji/guildEmojiUpdate.js
@@ -0,0 +1,27 @@
+const { LOG_MODS, BOT_LOGO, BOT_TEXTFOOTER} = require('../../../config.json');
+const { AuditLogEvent, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    name : 'emojiUpdate',
+    description : 'Logs when an emoji is updated.',
+    async execute (emoji) {
+        const fetchedLogs = await emoji.guild.fetchAuditLogs({
+            limit: 1,
+            type: AuditLogEvent.EmojiUpdate,
+        });
+
+        const EmojiUpdateLog = fetchedLogs.entries.first();
+
+        const EmojiUpdatedEmbed = new EmbedBuilder()
+            .setColor("LightGrey")
+            .setDescription(`<@${EmojiUpdateLog.executor.id}> updated an emoji.`)
+            .addFields( {name : "Name", value : `\`\`\`ini\nOld_name = ${EmojiUpdateLog.changes[0]['old']}\nNew_name = ${EmojiUpdateLog.changes[1]['new']}\`\`\``} )
+            .setFooter({ text: BOT_TEXTFOOTER, iconURL: BOT_LOGO })
+        
+        if(emoji.animated) {
+            EmojiUpdatedEmbed.setImage(`https://cdn.discordapp.com/emojis/${emoji.id}.gif`);
+        } else {
+            EmojiUpdatedEmbed.setImage(`https://cdn.discordapp.com/emojis/${emoji.id}.png`);
+        }
+    }
+}
