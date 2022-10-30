fb7b84507740de9d637d3abc26ff00d344f2c910
EAnathos <eanathos@gmail.com>
2022-10-30 19:53:53 +0100

    Create skip.js

---
+++ commands/musics/skip.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+const { EmbedBuilder } = require("discord.js");
+const { execute } = require("./play");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+        .setName("skip")
+        .setDescription("Skips the current song"),
+
+	async execute ({ client, interaction }) {
+
+		const queue = client.player.getQueue(interaction.guildId)
+
+		if (!queue)
+        {
+            await interaction.reply("There are no songs in the queue");
+            return;
+        }
+
+        const currentSong = queue.current
+
+		queue.skip()
+
+        const skipEmbed = new EmbedBuilder()
+            .setDescription(`${currentSong.title} has been skipped!`)
+            .setThumbnail(currentSong.thumbnail)
+        
+        await interaction.reply({embeds: [skipEmbed]});
+	},
+}
