3244890025a70a7157c121c5bbeb416ef0d24e27
EAnathos <eanathos@gmail.com>
2022-10-31 10:04:53 +0100

    Update skip.js

--- commands/musics/skip.js
+++ commands/musics/skip.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 const { EmbedBuilder } = require("discord.js");
-const { execute } = require("./play");
 
 module.exports = {
 	data: new SlashCommandBuilder()
