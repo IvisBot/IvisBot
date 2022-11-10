b7ad312d291ebc0d5f7bbface9be0af04f6bff7e
EAnathos <eanathos@gmail.com>
2022-11-10 22:17:35 +0100

    the interaction now create a channel

--- events/ticket/ticketCreate.js
+++ events/ticket/ticketCreate.js
@@ -1,5 +1,5 @@
-const { TICKET } = require('../../config.json');
-const { ActionRowBuilder, SelectMenuBuilder} = require('discord.js');
+const { TICKETS_CATEGORY, CLIENT_ID } = require('../../config.json');
+const { ActionRowBuilder, SelectMenuBuilder, PermissionsBitField } = require('discord.js');
 
 module.exports = {
     name : 'interactionCreate',
@@ -26,6 +26,48 @@ module.exports = {
                     );
                 
                 await interaction.reply({ ephemeral: true, components: [row] });
+            }
+        } else if (interaction.isSelectMenu()) {
+            if (interaction.customId === 'ticketCreate') {
+                const selected = interaction.values[0];
+
+                if (selected === 'first_option') {
+                    interaction.guild.channels.create({
+                        name : `ticket-${interaction.user.username}`,
+                        parent: TICKETS_CATEGORY,
+                        permissionOverwrites: [
+                            {
+                                id: interaction.guild.roles.everyone,
+                                deny: [PermissionsBitField.Flags.ViewChannel],
+                            },
+                            {
+                                id: interaction.user.id,
+                                allow: [
+                                    PermissionsBitField.Flags.ViewChannel,
+                                    PermissionsBitField.Flags.SendMessages,
+                                    PermissionsBitField.Flags.AttachFiles,
+                                    PermissionsBitField.Flags.AddReactions
+                                ],
+                            },
+                            {
+                                id: CLIENT_ID,
+                                allow: [
+                                    PermissionsBitField.Flags.ViewChannel,
+                                    PermissionsBitField.Flags.SendMessages,
+                                    PermissionsBitField.Flags.AttachFiles,
+                                    PermissionsBitField.Flags.AddReactions,
+                                    PermissionsBitField.Flags.ManageChannels,
+                                    PermissionsBitField.Flags.ManageMessages
+                                ],
+                            }
+                        ],
+                    }).then(async (channel) => {
+                        await channel.send({ content: `Hello ${interaction.user}!` });
+                    });
+                    await interaction.reply({ content: `You selected the [⚜️ role asking] option`, ephemeral: true });
+                } else {
+                    await interaction.reply({ content: `You selected the [🚩 Report] option`, ephemeral: true });
+                }
             }
         }
     }
